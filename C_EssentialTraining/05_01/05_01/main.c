b2fb85fccf934f9a836bddf8dce7928ab516db76
Jim Veneskey <github@strifecrafter.com>
2016-01-19 06:40:12 -0500

    Variable names - cryptic variable names sometimes make sense...

---
+++ C_EssentialTraining/05_01/05_01/main.c
@@ -0,0 +1,44 @@
+//
+//  main.c
+//  05_01
+//
+//  Created by jim Veneskey on 1/19/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Prototypes
+void cubeDimension(void);
+
+int main(void) {
+    
+    //Are the following variable names appropriate?
+    int x;
+    int y;
+    int z;
+    
+    //Are the following variable names appropriate?
+    char mylastknownaddress[30];
+    char myLastKnownAddress[30];
+    
+    //Are the above variable names identical?
+    
+    return EXIT_SUCCESS;
+}
+
+void cubeDimension(void){
+    //The following are the 3 dimensions of a cube
+    double x,y,z;
+}
+
+void myLastKnownAddress(void){
+    //Q: Is the name of this function appropriate?
+    //A: Yes, but...
+    
+    //Q: Can a function have the same name as a variable?
+    //A: Yes, but...
+    
+}
