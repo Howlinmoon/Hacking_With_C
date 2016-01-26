ad52cf8f5505d562103433bfab5f56df9a5dca9d
Jim Veneskey <github@strifecrafter.com>
2016-01-26 12:36:56 -0500

    comparing WHILE and DO WHILE loops

---
+++ C_EssentialTraining/09_04/09_04/main.c
@@ -0,0 +1,46 @@
+//
+//  main.c
+//  09_04 DO WHILE loop
+//
+//  Created by jim Veneskey on 1/26/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define TRUE 1;
+#define FALSE 0;
+
+// function prototypes
+void getInputData1(void);
+void getInputData2(void);
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    getInputData1();
+    //getInputData2();
+    
+    return EXIT_SUCCESS;
+}
+
+// functions
+
+// using WHILE loop
+
+void getInputData1(void) {
+    char c = 'x';
+    int testVariable = 0;
+    
+    while (c != 'x') {
+        testVariable = 999;
+        printf("Enter a letter please: ");
+        c = getchar();
+        printf("You entered: %c\n",c);
+        getchar();
+    }
+    
+    printf("You entered 'x' and the loop exited\n");
+    printf("testVariable = %d\n", testVariable);
+}
+
