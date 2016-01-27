47a532968707b613b9f13214b5ed03e200ba22d0
Jim Veneskey <github@strifecrafter.com>
2016-01-27 06:23:28 -0500

    My answer to the challenge

---
+++ C_EssentialTraining/10_01/10_01/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  10_01
+//
+//  Created by jim Veneskey on 1/27/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
