f7910a27c0f81d10e778fd62d679019303cf6532
Jim Veneskey <github@strifecrafter.com>
2016-01-27 06:35:27 -0500

    defining array sizes using constants

---
+++ C_EssentialTraining/10_02/10_02/constants.h
@@ -0,0 +1,15 @@
+/*
+ * constants.h
+ *
+ *  Created on: May 8, 2014
+ *      Author: isacartzi
+ */
+
+#ifndef CONSTANTS_H_
+#define CONSTANTS_H_
+
+#define MAX_SCORES      20
+#define MAX_LETTERS     10
+#define MAX_DATA_POINTS 50
+
+#endif /* CONSTANTS_H_ */
---
+++ C_EssentialTraining/10_02/10_02/main.c
@@ -0,0 +1,22 @@
+//
+//  main.c
+//  10_02 - alternative method to declaring arrays
+//
+//  Created by jim Veneskey on 1/27/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "constants.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    // defining array sizes using predefined constants
+    int  scores[MAX_SCORES];
+    char   word[MAX_LETTERS];
+    double data[MAX_DATA_POINTS];
+    
+    return EXIT_SUCCESS;
+}
