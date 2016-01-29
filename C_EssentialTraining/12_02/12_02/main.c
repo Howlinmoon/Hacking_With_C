35260b4c755135ab7d348a82b01e5e5f9f3be05f
Jim Veneskey <github@strifecrafter.com>
2016-01-29 06:47:37 -0500

    initializing and accessing elements of a structure

---
+++ C_EssentialTraining/12_02/12_02/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  12_02
+//
+//  Created by jim Veneskey on 1/29/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
