29c9810d06f2ad1737ca41269432023d1c30f048
Jim Veneskey <github@strifecrafter.com>
2016-02-01 12:58:39 -0500

    experimenting with text location

--- Curses/demo.c
+++ Curses/demo.c
@@ -5,6 +5,16 @@ int main(int argc, char *argv[]) {
   initscr();
   noecho();
   curs_set(FALSE);
-  sleep(1);
+
+  for (int x = 1; x <= 55; x++) {
+  	for (int y = 1; y <= 10; y++) {
+  		clear();
+	 	mvprintw(y, x, "Hello, X = %d, Y = %d", x, y);
+  		refresh();
+  		usleep(100000);
+  	}
+  }
+
+
   endwin(); // Restore normal terminal behavior
 }
