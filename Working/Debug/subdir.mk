06ef0186909319e52e19aab21ca9606face9e5be
Jim Veneskey <github@strifecrafter.com>
2016-02-05 13:05:03 -0500

    Starting C++ Essential Training

--- Working/Debug/subdir.mk
+++ Working/Debug/subdir.mk
@@ -3,21 +3,21 @@
 ################################################################################
 
 # Add inputs and outputs from these tool invocations to the build variables 
-C_SRCS += \
-../00_04.c 
+CPP_SRCS += \
+../version-test.cpp 
 
 OBJS += \
-./00_04.o 
+./version-test.o 
 
-C_DEPS += \
-./00_04.d 
+CPP_DEPS += \
+./version-test.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-%.o: ../%.c
+%.o: ../%.cpp
 	@echo 'Building file: $<'
-	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
