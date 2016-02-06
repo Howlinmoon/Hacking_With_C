2b73342d58078869ef838dd1da57709c67545545
Howlinmoon <github@strifecrafter.com>
2016-02-06 11:14:44 -0500

    Working with C++

---
+++ CppEssentialTraining/02_06/02_06/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  02_06
+//
+//  Created by Jim Veneskey on 2/6/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main( int argc, char ** argv ) {
+    printf("Hello, World!\n");
+    return 0;
+}
+
