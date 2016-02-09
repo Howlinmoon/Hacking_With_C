427b2315503fc3513cac58ccd88af0525549c737
Jim Veneskey <github@strifecrafter.com>
2016-02-09 06:03:54 -0500

    defining and initializing variables

---
+++ CppEssentialTraining/03_03/03_03/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  03_03 - defining variables
+//
+//  Created by jim Veneskey on 2/9/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    int i; // declaring a simple variable, but does not initialize it
+    i = 7; // now it is initialized/defined.
+    char *s = "Hello World, pointer style!\n";
+    printf(s);
+    return 0;
+}
