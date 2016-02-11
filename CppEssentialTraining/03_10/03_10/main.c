78e0f2f7ca9d3ecc15f147e597ee8bcc4c691568
Jim Veneskey <github@strifecrafter.com>
2016-02-11 06:48:14 -0500

    experimenting with switch statements

---
+++ CppEssentialTraining/03_10/03_10/main.c
@@ -0,0 +1,31 @@
+//
+//  main.c
+//  03_10 - using switch statements
+//
+//  Created by jim Veneskey on 2/11/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+
+    int foo = 3;
+    switch (foo) {
+        case 1:
+            printf("one\n");
+            break;
+        case 2:
+            printf("two\n");
+            break;
+        case 3:
+            printf("three\n");
+            break;
+        case 4:
+            printf("four\n");
+            break;
+    }
+    
+    return 0;
+}
