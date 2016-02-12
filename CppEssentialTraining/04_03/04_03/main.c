5336cad83211cf63cf3901de33da5c8bc40b2c6d
Jim Veneskey <github@strifecrafter.com>
2016-02-12 06:34:58 -0500

    experimenting with include files

---
+++ CppEssentialTraining/04_03/04_03/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  04_03 include files
+//
+//  Created by jim Veneskey on 2/12/16.
+//  Copyright © 2016 Jim Veneskey. All rights reserved.
+//
+
+// including a system common header file
+#include <stdio.h>
+// adding in a project specific header file
+#include "preproc.h"
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    printf("The constants is %d\n", _iOne);
+    return 0;
+}
